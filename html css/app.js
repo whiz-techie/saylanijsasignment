03523a4b224604f1180a027f200b7828e4de312d
whiz-techie <57448998+whiz-techie@users.noreply.github.com>
2020-06-22 13:59:19 +0500

    Add files via upload

---
+++ html css/app.js
@@ -0,0 +1,42 @@
+
+
+
+
+// function getName(){
+//     var name = document.getElementById("name");
+//     alert(name.value);
+
+//     name.value = ""
+// }
+
+function getNumber(num){
+
+    var result = document.getElementById("result");
+
+    result.value += num;
+}
+
+function ClearResult(){
+    var result = document.getElementById("result");
+        result.value = "";
+}
+
+function GetResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
